cd75d04e3079c85d148c7838931469eea90572ee
Thomas R. Honeyman <admin@thomashoneyman.com>
2017-12-18 21:01:40 -0800

    FIX: Correct location of examples script for circle CI

--- .circleci/run-examples.sh
+++ .circleci/run-examples.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cd ../examples
+
+# In Circle CI this runs from the project root, so there is no need
+# to navigate up a directory before moving into the /examples folder.
+cd examples
+
 for d in */ ; do
     testname="example-$d"
     testname="${testname%?}"  # truncate last character
