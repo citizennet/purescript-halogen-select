aac003ecc09c170e9b86850680e7bb606ce7a55d
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-05-18 20:42:08 -0700

    Cleanup examples by putting input and specs in a where clause

--- examples/Components/Dropdown.purs
+++ examples/Components/Dropdown.purs
@@ -33,52 +33,54 @@ type Input =
   }
 
 component :: H.Component HH.HTML S.Query' Input Message Aff
-component = S.component input spec
-
-input :: Input -> S.Input State
-input { items, buttonLabel } =
-  { inputType: S.Toggle
-  , search: Nothing
-  , debounceTime: Nothing
-  , getItemCount: length <<< _.items
-  , items
-  , buttonLabel
-  , selection: Nothing
-  }
-
-spec :: S.Spec State (Const Void) Void () Message Aff
-spec = S.defaultSpec { render = render, handleMessage = handleMessage }
+component = S.component input $ S.defaultSpec
+    { render = render
+    , handleMessage = handleMessage
+    }
   where
-  handleMessage = case _ of
-    S.Selected ix -> do
-      st <- H.get
-      let selection = st.items !! ix
-      H.modify_ _ { selection = selection, visibility = S.Off }
-      H.raise $ SelectionChanged st.selection selection
-    _ -> pure unit
+    input :: Input -> S.Input State
+    input { items, buttonLabel } =
+      { inputType: S.Toggle
+      , search: Nothing
+      , debounceTime: Nothing
+      , getItemCount: length <<< _.items
+      , items
+      , buttonLabel
+      , selection: Nothing
+      }
 
-  render st =
-    HH.div
-      [ class_ "Dropdown" ]
-      [ renderToggle, renderContainer ]
-    where
-    renderToggle =
-      HH.button
-        ( SS.setToggleProps [ class_ "Dropdown__toggle" ] )
-        [ HH.text (fromMaybe st.buttonLabel st.selection) ]
+    handleMessage :: S.Message -> H.HalogenM (S.State State) S.Action' Message Aff Unit
+    handleMessage = case _ of
+      S.Selected ix -> do
+        st <- H.get
+        let selection = st.items !! ix
+        H.modify_ _ { selection = selection, visibility = S.Off }
+        H.raise $ SelectionChanged st.selection selection
+      _ -> pure unit
 
-    renderContainer = whenElem (st.visibility == S.On) \_ ->
+    render :: S.State State -> H.ComponentHTML S.Action' () Aff
+    render st =
       HH.div
-        ( SS.setContainerProps [ class_ "Dropdown__container" ] )
-        ( renderItem `mapWithIndex` st.items )
+        [ class_ "Dropdown" ]
+        [ renderToggle, renderContainer ]
       where
-      renderItem index item =
+      renderToggle =
+        HH.button
+          ( SS.setToggleProps [ class_ "Dropdown__toggle" ] )
+          [ HH.text (fromMaybe st.buttonLabel st.selection) ]
+
+      renderContainer = whenElem (st.visibility == S.On) \_ ->
         HH.div
-          ( SS.setItemProps index
-              [ classes_
-                  [ "Dropdown__item"
-                  , "Dropdown__item--highlighted" # guard (st.highlightedIndex == Just index)
-                  ]
-              ]
-          )
-          [ HH.text item ]
+          ( SS.setContainerProps [ class_ "Dropdown__container" ] )
+          ( renderItem `mapWithIndex` st.items )
+        where
+        renderItem index item =
+          HH.div
+            ( SS.setItemProps index
+                [ classes_
+                    [ "Dropdown__item"
+                    , "Dropdown__item--highlighted" # guard (st.highlightedIndex == Just index)
+                    ]
+                ]
+            )
+            [ HH.text item ]
